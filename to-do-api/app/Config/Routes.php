306b0358b279ef9fd268a271206c40689eb7acec
Hendry Winata <hendry24680@gmail.com>
2022-06-04 11:13:44 +0700

    update item endpoint route

--- to-do-api/app/Config/Routes.php
+++ to-do-api/app/Config/Routes.php
@@ -36,7 +36,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->resource('item');
+$routes->resource('item', ['controller' => 'Item']);
 
 /*
  * --------------------------------------------------------------------
